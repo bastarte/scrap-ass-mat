ff6369b62642d44c30317439320ce61829338d39
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-04-03 20:15:00 +0200

    handle many pages + segregate CSV save

--- controller.rb
+++ controller.rb
@@ -1,6 +1,28 @@
 class Controller
+
   def import_from_web
-    data = call
-    save_CSV(data)
+    assmats = (1..10).to_a
+    page = 0
+    all_assmats = []
+    while assmats.length == 10
+      attributes = { start: page * 10 }
+      page += 1
+      assmats = call(attributes)
+      pp assmats
+      all_assmats += assmats
+    end
+    save_CSV(all_assmats)
+  end
+
+  private
+
+  def save_CSV(assmats)
+    pp assmats
+    CSV.open("data.csv", "wb") do |csv|
+      csv << assmats.first.keys # adds the attributes name on the first line
+      assmats.each do |hash|
+        csv << hash.values
+      end
+    end
   end
 end
--- scrapper.rb
+++ scrapper.rb
@@ -2,12 +2,11 @@
 # url = 'https://assmat.loire-atlantique.fr/jcms/parents/faire-une-recherche-d-assistante-maternelle-fr-r1_58176?idCommune=rp1_62646&codeInsee=44109&cities=44036&longitude=-1.56512&latitude=47.219901&cityName=Nantes&adresse=Passage+Louis+L%C3%A9vesque+44000+Nantes&distance=3000&month=1617200000000&age=1%7C17%7C2%7C3%7C10%7C15%7C16%7C19&branchesId=cra_67000&branchesId=cra_67001&branchesId=&nomassmat=&isSearch=Ok&hashKey=88&withDispo=false&withDispoFuture=true&withNonDispo=false&withDispoNonRenseigne=false'
 
 # url = 'https://assmat.loire-atlantique.fr/jcms/parents/faire-une-recherche-d-assistante-maternelle-fr-r1_58176?idCommune=rp1_62646&codeInsee=44109&cities=44036&longitude=-1.56512&latitude=47.219901&cityName=Nantes&adresse=Passage+Louis+L%C3%A9vesque+44000+Nantes&distance=3000&month=1617200000000&age=1%7C17%7C2%7C3%7C10%7C15%7C16%7C19&branchesId=cra_67000&branchesId=cra_67001&branchesId=&nomassmat=&isSearch=Ok&hashKey=88&withDispo=true&withDispoFuture=false&withNonDispo=false&withDispoNonRenseigne=false'
-START = [0, 10, 20, 30, 40, 50, 60]
 
-def call
-  url = "https://assmat.loire-atlantique.fr/jcms/parents/faire-une-recherche-d-assistante-maternelle-fr-r1_58176?idCommune=rp1_62646&codeInsee=44109&cities=44036&longitude=-1.56512&latitude=47.219901&cityName=Nantes&adresse=Passage+Louis+L%C3%A9vesque+44000+Nantes&distance=3000&month=1617200000000&age=1%7C17%7C2%7C3%7C10%7C15%7C16%7C19&branchesId=cra_67000&branchesId=cra_67001&branchesId=&nomassmat=&isSearch=Ok&hashKey=88&withDispo=true&withDispoFuture=true&withNonDispo=false&withDispoNonRenseigne=false&start=0"
+def call(attributes = {start: 0})
+  url = "https://assmat.loire-atlantique.fr/jcms/parents/faire-une-recherche-d-assistante-maternelle-fr-r1_58176?idCommune=rp1_62646&codeInsee=44109&cities=44036&longitude=-1.56512&latitude=47.219901&cityName=Nantes&adresse=Passage+Louis+L%C3%A9vesque+44000+Nantes&distance=3000&month=1617200000000&age=1%7C17%7C2%7C3%7C10%7C15%7C16%7C19&branchesId=cra_67000&branchesId=cra_67001&branchesId=&nomassmat=&isSearch=Ok&hashKey=88&withDispo=true&withDispoFuture=true&withNonDispo=false&withDispoNonRenseigne=false&start=#{attributes[:start]}"
 
-  unless File.file?('document.html')
+  unless false # File.file?('document.html')
     html_file = open(url).read
     html_doc = Nokogiri::HTML(html_file)
     File.write('document.html', html_doc.search('.amcontainer'))
@@ -36,14 +35,4 @@ def call
     assmats << assmat
   end
   assmats
-end
-
-def save_CSV(assmats)
-  pp assmats
-  CSV.open("data.csv", "wb") do |csv|
-    csv << assmats.first.keys # adds the attributes name on the first line
-    assmats.each do |hash|
-      csv << hash.values
-    end
-  end
 end
